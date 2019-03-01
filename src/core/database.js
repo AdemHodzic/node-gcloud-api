3344bdfaa20af56b6f43a59dfb158f18577e58b3
Hodzic Adem <athos.blade@gmail.com>
2019-03-01 19:41:21 +0100

    Deployed successfully

--- src/core/database.js
+++ src/core/database.js
@@ -1,23 +1,32 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 
-const dbName = process.env.SQL_DATABASE;
-const dbUser = process.env.SQL_USER;
-const dbPassword = process.env.SQL_PASSWORD;
-const dbHost = process.env.SQL_HOST;
+const dbName = process.env.SQL_DATABASE
+const dbUser = process.env.SQL_USER
+const dbPassword = process.env.SQL_PASSWORD
+// const dbHost = process.env.SQL_HOST
+const dbSocket = process.env.INSTANCE_CONNECTION_NAME || ''
 
 const database = new Sequelize(dbName, dbUser, dbPassword, {
-  host: dbHost,
+  host: `/cloudsql/${dbSocket}`,
   dialect: 'mysql',
-  operatorsAliases: false,
-});
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  dialectOptions: {
+    socketPath: `/cloudsql/${dbSocket}`
+  }
+})
 
 database
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log('Connection has been established successfully.')
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+    console.error('Unable to connect to the database:', err)
+  })
 
-module.exports = database;
+module.exports = database
